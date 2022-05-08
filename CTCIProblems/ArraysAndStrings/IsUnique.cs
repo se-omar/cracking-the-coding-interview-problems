d5eeef35e989084cb1ed72ac13c281a1c213de08
se-omarayman <se.omarayman@gmail.com>
2022-05-08 03:04:32 +0200

    finished IsUnique.

---
+++ CTCIProblems/ArraysAndStrings/IsUnique.cs
@@ -0,0 +1,33 @@
+﻿namespace CTCIProblems.ArraysAndStrings;
+
+public class IsUnique
+{
+    //this assumes 128 character ascii
+    //O(n) time, O(1) space.
+    public bool Solution(string s)
+    {
+        if (s.Length > 128) return false;
+        bool[] occurrences = new bool[128];
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (occurrences[s[i]]) return false;
+                occurrences[s[i]] = true;
+        }
+
+        return true;
+
+    }
+
+    public bool BruteforceSolution(string s)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int j = i + 1; j < s.Length; j++)
+            {
+                if (s[i] == s[j]) return false;
+            }
+        }
+        
+        return true;
+    }
+}
---
+++ CTCIProblems/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using CTCIProblems.ArraysAndStrings;
+
+Console.WriteLine("Hello, World!");
+
+IsUnique isUnique = new();
+bool answer = isUnique.Solution("abcdefgg");
+Console.WriteLine(answer);
