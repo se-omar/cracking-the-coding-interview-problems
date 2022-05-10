8da50f7700ec45a1b04a3d198125e3436fbad054
se-omarayman <se.omarayman@gmail.com>
2022-05-10 21:40:28 +0200

    finished CheckPermutation

---
+++ CTCIProblems/ArraysAndStrings/CheckPermutation.cs
@@ -0,0 +1,25 @@
+﻿namespace CTCIProblems.ArraysAndStrings;
+
+public class CheckPermutation
+{
+    // o(n) time, 
+    public bool Solution(string strA, string strB)
+    {
+        int[] occurs = new int[128];
+        
+        for (int i = 0; i < strA.Length; i++)
+        {
+            int charVal = strA[i];
+            occurs[charVal]++;
+        }
+        
+        for (int i = 0; i < strB.Length; i++)
+        {
+            int charVal = strB[i];
+            occurs[charVal]--;
+            if (occurs[charVal] < 0) return false;
+        }
+
+        return true;
+    }
+}
