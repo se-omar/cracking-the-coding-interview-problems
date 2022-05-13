f03fc780b764162d752d8b0c7a42214f64b06a61
se-omarayman <se.omarayman@gmail.com>
2022-05-13 17:53:28 +0200

    finished PalindromePermutation

---
+++ CTCIProblems/ArraysAndStrings/PalindromePermutation.cs
@@ -0,0 +1,31 @@
+﻿namespace CTCIProblems.ArraysAndStrings;
+
+public class PalindromePermutation
+{
+    //O(N) time, O(1) space. 
+    public bool Solution(string s)
+    {
+        bool oddFound = false;
+        int[] occurs = new int[26];
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            int charVal = s[i];
+            occurs[charVal - 'a']++;
+        }
+
+        for (int i = 0; i < occurs.Length; i++)
+        {
+            if (occurs[i] != 0)
+            {
+                if (occurs[i] % 2 != 0)
+                {
+                    if (oddFound) return false;
+                    oddFound = true;
+                }
+            }
+        }
+
+        return true;
+    }
+}
--- CTCIProblems/Program.cs
+++ CTCIProblems/Program.cs
@@ -2,7 +2,5 @@
 using CTCIProblems.ArraysAndStrings;
 
 Console.WriteLine("Hello, World!");
-
-IsUnique isUnique = new();
-bool answer = isUnique.Solution("abcdefgg");
-Console.WriteLine(answer);
+PalindromePermutation palindromePermutation = new();
+palindromePermutation.Solution("tact coa");
