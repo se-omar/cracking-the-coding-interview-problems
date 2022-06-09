b46c9dcdfaaedd1b7ebd7f22c2411d1bf0b7ab55
se-omarayman <se.omarayman@gmail.com>
2022-06-09 19:02:59 +0200

    finished RotateMatrix

---
+++ CTCIProblems/ArraysAndStrings/RotateMatrix.cs
@@ -0,0 +1,31 @@
+﻿namespace CTCIProblems.ArraysAndStrings;
+
+public class RotateMatrix
+{
+    public int[][] Solution(int[][] sourceAr)
+    {
+        int left = 0;
+        int right = sourceAr.Length - 1;
+
+        while (left < right)
+        {
+            //loop number of layers (n/2)
+            for (int i = 0; i < right - left; i++)
+            {
+                int top = left;
+                int bottom = right;
+
+                int topLeft = sourceAr[top][left + i];
+                sourceAr[top][left + i] = sourceAr[bottom - i][left];
+                sourceAr[bottom - i][left] = sourceAr[bottom][right - i];
+                sourceAr[bottom][right - i] = sourceAr[top + i][right];
+                sourceAr[top + i][right] = topLeft;
+            }
+
+            left++;
+            right--;
+        }
+
+        return sourceAr;
+    }
+}
--- CTCIProblems/Program.cs
+++ CTCIProblems/Program.cs
@@ -1,6 +1,12 @@
-﻿
-using CTCIProblems.ArraysAndStrings;
+﻿using CTCIProblems.ArraysAndStrings;
 
-Console.WriteLine("Hello, World!");
-PalindromePermutation palindromePermutation = new();
-palindromePermutation.Solution("tact coa");
+
+int[][] matrix = new int[3][];
+
+    matrix[0] = new[] {1, 2, 3};
+    matrix[1] = new[] {4, 5, 6};
+    matrix[2] = new[] {7, 8, 9};
+
+
+RotateMatrix rotateMatrix = new();
+Console.WriteLine(rotateMatrix.Solution(matrix));
