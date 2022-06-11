9e8535f1857c93e8ea1239a993be8db9cd8bfab9
se-omarayman <se.omarayman@gmail.com>
2022-06-11 14:02:49 +0200

    finished ZeroMatrix

---
+++ CTCIProblems/ArraysAndStrings/ZeroMatrix.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+
+namespace CTCIProblems.ArraysAndStrings;
+
+public class ZeroMatrix
+{
+    public int[][] Solution(int[][] mat, int rowCount, int colCount)
+    {
+        bool[] rowsToBeZero = new bool[rowCount];
+        bool[] colsToBeZero = new bool[colCount];
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < colCount; j++)
+            {
+                if (mat[i][j] == 0)
+                {
+                    rowsToBeZero[i] = true;
+                    colsToBeZero[j] = true;
+                }
+            }
+        }
+
+        ZeroRows(mat, rowsToBeZero, colCount);
+        ZeroCols(mat, colsToBeZero, rowCount);
+
+        return mat;
+    }
+
+    public void ZeroRows(int[][] mat, bool[] rowsToBeZero, int colCount)
+    {
+        for (int i = 0; i < rowsToBeZero.Length; i++)
+        {
+            if (rowsToBeZero[i])
+            {
+                for (int j = 0; j < colCount; j++) mat[i][j] = 0;
+            }
+        }
+    }
+
+    public void ZeroCols(int[][] mat, bool[] colsToBeZero, int rowCount)
+    {
+        for (int i = 0; i < colsToBeZero.Length; i++)
+        {
+            if (colsToBeZero[i])
+            {
+                for (int j = 0; j < rowCount; j++) mat[j][i] = 0;
+            }
+        }
+    }
+}
--- CTCIProblems/Program.cs
+++ CTCIProblems/Program.cs
@@ -5,8 +5,7 @@ int[][] matrix = new int[3][];
 
     matrix[0] = new[] {1, 2, 3};
     matrix[1] = new[] {4, 5, 6};
-    matrix[2] = new[] {7, 8, 9};
+    matrix[2] = new[] {0, 8, 9};
 
-
-RotateMatrix rotateMatrix = new();
-Console.WriteLine(rotateMatrix.Solution(matrix));
+ZeroMatrix zeroMatrix = new();
+zeroMatrix.Solution(matrix, 3, 3);
