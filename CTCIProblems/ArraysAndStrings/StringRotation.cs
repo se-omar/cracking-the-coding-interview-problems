cd2cec1f7ecf9e1a443144593a53119d42386e19
se-omarayman <se.omarayman@gmail.com>
2022-06-12 12:57:57 +0200

    finished StringRotation

---
+++ CTCIProblems/ArraysAndStrings/StringRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace CTCIProblems.ArraysAndStrings;
+// time O(N), space O(1)
+public class StringRotation
+{
+    public bool Solution(string s1, string s2)
+    {
+        string s1s1 = s1 + s1;
+        if (s1.Length != s2.Length || s1.Length == 0 || s2.Length == 0) return false;
+        return s1s1.Contains(s2);
+    }
+}
--- CTCIProblems/Program.cs
+++ CTCIProblems/Program.cs
@@ -1,11 +1,11 @@
 ﻿using CTCIProblems.ArraysAndStrings;
 
 
-int[][] matrix = new int[3][];
+// int[][] matrix = new int[3][];
+//
+//     matrix[0] = new[] {1, 2, 3};
+//     matrix[1] = new[] {4, 5, 6};
+//     matrix[2] = new[] {0, 8, 9};
 
-    matrix[0] = new[] {1, 2, 3};
-    matrix[1] = new[] {4, 5, 6};
-    matrix[2] = new[] {0, 8, 9};
-
-ZeroMatrix zeroMatrix = new();
-zeroMatrix.Solution(matrix, 3, 3);
+StringRotation stringRotation = new();
+stringRotation.Solution("waterbottle", "erbottlewat");
